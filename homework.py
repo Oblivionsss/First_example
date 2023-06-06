52a1038b59feb639cec492fc9d567072021536b2
NevolinAlexandrr <nevolin_alexadnrr@mail.ru>
2023-06-06 23:15:47 +0300

    Create class Record and Calculator

--- homework.py
+++ homework.py
@@ -1,22 +1,28 @@
 import datetime as dt
         
 class Record:
-     ...
-        
+    def __init__(self,amount, date, comment):
+        self.amount = amount
+        self.date = date
+        self.comment = comment
+
+
 class Calculator:
-     ...
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []     
         
 # создадим калькулятор денег с дневным лимитом 1000
-cash_calculator = CashCalculator(1000)
+# cash_calculator = CashCalculator(1000)
         
 # дата в параметрах не указана, 
 # так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
+# cash_calculator.add_record(Record(amount=145, comment="кофе")) 
 # и к этой записи тоже дата должна добавиться автоматически
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
+# cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
 # а тут пользователь указал дату, сохраняем её
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
+# cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date="08.11.2019"))
                 
-print(cash_calculator.get_today_cash_remained("rub"))
+# print(cash_calculator.get_today_cash_remained("rub"))
 # должно напечататься
 # На сегодня осталось 555 руб
