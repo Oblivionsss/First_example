037fe6e09c3eb1e0470e77324622f0dad9950144
NevolinAlexandrr <nevolin_alexadnrr@mail.ru>
2023-06-08 23:27:17 +0300

    Create function get_week_stats() in parent class Calculator

--- homework.py
+++ homework.py
@@ -22,17 +22,34 @@ class Calculator:
         """     
         self.records.append([records.amount, records.comment, records.date])
 
- 
+
+    def get_today(self):
+        return dt.datetime.today().date()
+    
     def get_today_stats(self):
         """
         Функция возвращает сумму калорий / денег
         потраченных за сегодняшний день
         today = дата на сегодня
         """
-        today = dt.datetime.today().date()
+        today = self.get_today()
         return sum([i[0] 
                 for i in self.records 
                 if dt.datetime.strptime(i[2], self.format).date() == today])
+
+    def get_week_stats(self):
+        """
+        Функция возвращает сумму калорий / денег
+        потраченных за последнюю неделю
+        today = дата сегодня
+        first date = дата семь дней назад
+        """
+        today = self.get_today()
+        first_date = today - dt.timedelta(days=7)
+        return sum([i[0]
+                for i in self.records if first_date
+                < dt.datetime.strptime(i[2], self.format).date()
+                <= today])
 
 
 class CashCalculator(Calculator):
@@ -53,9 +70,13 @@ cash_calculator = Calculator(1000)
 cash_calculator.add_record(Record(amount=100, comment="кофе", date="08.06.2023"))
 cash_calculator.add_record(Record(amount=111, comment="булка", date="08.06.2023"))
 cash_calculator.add_record(Record(amount=111, comment="пятерочка", date="08.06.2023"))
+cash_calculator.add_record(Record(amount=120, comment="магазин", date="02.06.2023"))
+cash_calculator.add_record(Record(amount=120, comment="магазин", date="30.05.2023"))
+cash_calculator.add_record(Record(amount=120, comment="магазин", date="09.06.2023"))
 cash_calculator.add_record(Record(amount=120, comment="магазин", date="09.06.2023"))
 print(cash_calculator.records)
 print(cash_calculator.get_today_stats())
+print(cash_calculator.get_week_stats())
 # и к этой записи тоже дата должна добавиться автоматически
 # cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
 # а тут пользователь указал дату, сохраняем её
